860209ce7d5e11bbead488ef5b48eccefb7fbf73
Martchus <martchus@gmx.net>
2022-02-02 22:31:14 +0100

    Add property to set default color/size of QuickImageProvider

--- qtquickforkawesome/imageprovider.h
+++ qtquickforkawesome/imageprovider.h
@@ -15,6 +15,9 @@ namespace QtForkAwesome {
 class Renderer;
 
 class QT_QUICK_FORK_AWESOME_EXPORT QuickImageProvider : public QQuickImageProvider {
+    Q_PROPERTY(QColor defaultColor defaultColor name WRITE setDefaultColor)
+    Q_PROPERTY(QSize defaultColor defaultSize name WRITE setDefaultSize)
+
 public:
     QuickImageProvider(const Renderer &renderer, const QColor &defaultColor = QColor(), const QSize &defaultSize = QSize(64, 64),
         QQuickImageProvider::ImageType type = QQuickImageProvider::Pixmap);
@@ -26,11 +29,38 @@ public:
     QPixmap requestPixmap(const QString &id, QSize *size, const QSize &requestedSize) override;
 #endif
 
+    QColor defaultColor() const;
+    QSize defaultSize() const;
+
+public Q_SLOTS:
+    void setDefaultColor(const QColor &color);
+    void setDefaultSize(const QSize &size);
+
 private:
     const Renderer &m_renderer;
     QColor m_defaultColor;
     QSize m_defaultSize;
 };
+
+inline QColor QuickImageProvider::defaultColor() const
+{
+    return m_defaultColor;
+}
+
+inline QSize QuickImageProvider::defaultSize() const
+{
+    return m_defaultSize;
+}
+
+inline void QuickImageProvider::setDefaultColor(const QColor &color)
+{
+    m_defaultColor = color;
+}
+
+inline void QuickImageProvider::setDefaultSize(const QSize &size)
+{
+    m_defaultSize = size;
+}
 
 } // namespace QtForkAwesome
 
