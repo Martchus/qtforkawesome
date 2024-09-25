Martchus/qtforkawesome

commit 14403eeb0f992b3984ea29b0f389a813432c0327

Print both error messages in case no Perl YAML module can be loaded
This is useful to debug why the XS module cannot be loaded, e.g in cases
like https://github.com/Martchus/PKGBUILDs/issues/192.

qtforkawesome/scripts/yaml2enum.pl
@@ -4,10 +4,14 @@ use warnings;
 use utf8;
 
 # use either YAML::XS or YAML
-my $parse_yaml
-  =  eval { require YAML::XS; \&YAML::XS::Load }
-  // eval { require YAML;     \&YAML::Load     };
-die "Unable to locate YAML::XS or YAML: $@" if $@;
+my @errors;
+my $parse_yaml = eval { require YAML::XSS; \&YAML::XS::Load };
+push @errors, $@ if $@;
+if (!defined $parse_yaml) {
+    $parse_yaml = eval { require YAML; \&YAML::Load };
+    push @errors, $@ if $@;
+}
+die "Unable to locate YAML::XS or YAML:\n" . join('', @errors) unless defined $parse_yaml;
 
 # read CLI args
 my $yaml_file        = $ARGV[0] // '';

commit 8f4a465a8b744b9ca25133a6317a5517dfdc7b86

Fix typo introduced in 14403eeb0

qtforkawesome/scripts/yaml2enum.pl
@@ -5,7 +5,7 @@ use utf8;
 
 # use either YAML::XS or YAML
 my @errors;
-my $parse_yaml = eval { require YAML::XSS; \&YAML::XS::Load };
+my $parse_yaml = eval { require YAML::XS; \&YAML::XS::Load };
 push @errors, $@ if $@;
 if (!defined $parse_yaml) {
     $parse_yaml = eval { require YAML; \&YAML::Load };